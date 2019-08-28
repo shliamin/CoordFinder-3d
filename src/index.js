c2e37071bcc0d295360a12968c5f4c9ca0be7a6b
Efim Shliamin <shliamin@icloud.com>
2019-08-28 15:09:49 +0300

    new style created

--- src/index.js
+++ src/index.js
@@ -9,28 +9,27 @@ var renderer = new THREE.WebGLRenderer();
 var controls = new THREE.OrbitControls( camera);
 controls.addEventListener('change',render);
 
-renderer.setClearColor(0xdddddd);
+renderer.setClearColor(0x000000);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMapSoft = true;
 
-var axis = new THREE.AxesHelper(10);
+var axis = new THREE.AxesHelper(30);
 scene.add(axis);
 
-var grid = new THREE.GridHelper(50, 15);
+var grid = new THREE.GridHelper(50, 50);
 var color = new THREE.Color("rgb(255,0,0)");
 // grid.setColors(color,0x000000);
 // var colorfulGrid = new THREE.Mesh(grid, color);
 
-
 scene.add(grid);
 
 
-var cubeGeometry = new THREE.BoxGeometry(5,5,5);
+var cubeGeometry = new THREE.BoxGeometry(6,6,6);
 var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff3300});
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-var planeGeometry = new THREE.PlaneGeometry(30,30,30);
+var planeGeometry = new THREE.PlaneGeometry(50,50,50);
 var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
@@ -40,7 +39,7 @@ plane.receiveShadow = true;
 scene.add(plane);
 
 cube.position.x = 0;
-cube.position.y = 2.5;
+cube.position.y = 3;
 cube.position.z = 0;
 cube.castShadow = true;
 
@@ -104,23 +103,22 @@ window.addEventListener('mousemove', function(e){
 
   // console.log(vec.x);
 
-  document.getElementById('x-cam-to-cursor').textContent = vec.x;
+  // document.getElementById('x-cam-to-cursor').textContent = vec.x.toFixed(1);
   // document.getElementById('y-cam-to-cursor').textContent = vec.y;
-  document.getElementById('z-cam-to-cursor').textContent = vec.z;
+  // document.getElementById('z-cam-to-cursor').textContent = vec.z.toFixed(1);
 
-  document.getElementById('x-camera').textContent = camera.position.x;
+  // document.getElementById('x-camera').textContent = camera.position.x.toFixed(1);
   // document.getElementById('y-camera').textContent = camera.position.y;
-  document.getElementById('z-camera').textContent = camera.position.z;
+  // document.getElementById('z-camera').textContent = camera.position.z.toFixed(1);
 
-  document.getElementById('x-obj').textContent = camera.position.x - vec.x;
+  var objX = camera.position.x - vec.x;
+  var objZ = camera.position.z - vec.z;
+  document.getElementById('x-obj').textContent = objX.toFixed(1);
   // document.getElementById('y-obj').textContent = camera.position.y - vec.y;
-  document.getElementById('z-obj').textContent = camera.position.z - vec.z;
+  document.getElementById('z-obj').textContent = objZ.toFixed(1);
 
   });
 
-
-
-
-
 };
+
 
